79e153b5f5f968bccb74e66e43c96f46e9ba9a85
Chris <92526998+Hendrix8@users.noreply.github.com>
2022-03-02 18:06:10 +0200

    finding A

--- Lab_1/tem2405_Lab1b.py
+++ Lab_1/tem2405_Lab1b.py
@@ -4,15 +4,31 @@ from numpy import linalg as la
 g = open("graph0.txt", "r") 
 
 # Extracting the information of the file 
-lines = g.readlines() #['1 2\n', '1 3\n', '1 4\n', '2 3\n', '2 4\n', '3 1\n', '4 1\n', '4 3\n']
+lines = g.readlines() # example ['1 2\n', '1 3\n', '1 4\n', '2 3\n', '2 4\n', '3 1\n', '4 1\n', '4 3\n']
 L = []
 for i in lines:
-    L.append( ( int(i[0]), int(i[2])) ) # getting only 0th and 2nd element from lines 
+    L.append( (int(i[0]), int(i[2])) ) # getting only 0th and 2nd element from lines 
+    # L also represents the positions in which the items 1/L(j) go in the form (column, row)
 
 dimA = len(dict(L)) # the dimensions of A are the card of first items of the L list. (dict deletes duplicates)
 
-A = np.zeros(dimA,dimA)
+
+# creating the L(j) function
+def Lj(L, j):
+    cnt = 0
+    for tupl in L:
+        if j == tupl[0]:
+            cnt += 1
+        else:
+            pass 
+    return cnt
+
+A = np.zeros((dimA,dimA)) # creating an empty A to later add the elements
+
 # creating matrix A 
+for tupl in L:
+    A[tupl[1] - 1][tupl[0] - 1] = 1 / Lj(L, tupl[0]) # adding in the form (column,row) as L represents
+
 print(A)
     
 
